6a3612d18a33ac3a6a7fc287fe8464d6271f378b
luigi <luigi@localhost>
2020-11-30 01:21:11 -0300

    First Commit

---
+++ src/main/java/br/com/luisfga/spring/demo/IndexController.java
@@ -0,0 +1,41 @@
+package br.com.luisfga.spring.demo;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+@Controller
+public class IndexController {
+    
+    @GetMapping(value = {"","/","index","home"})
+    public String index(){
+        return "index";
+    }
+    
+    @GetMapping("/requestLocale")
+    public String requestLocale(
+            @RequestParam(name="lang", required=true, defaultValue="en") String lang,
+            HttpServletRequest request, 
+            HttpServletResponse response){
+        
+        LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+        localeResolver.setLocale(request, response, StringUtils.parseLocaleString(lang));
+        
+        return "redirect:" + request.getHeader("Referer");
+    }
+    
+    @GetMapping("login")
+    public String login(){
+        return "login";
+    }
+    
+    @GetMapping("dashboard")
+    public String dashboard(){
+        return "dashboard";
+    }
+}
